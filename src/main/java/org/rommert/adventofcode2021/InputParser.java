3d2b96ae19e0a8a262caff4a5dd433100efeb3c5
Rommert de Bruijn <Rommert.deBruijn@triodos.com>
2021-12-06 20:23:11 +0100

    Day4 part1

--- src/main/java/org/rommert/adventofcode2021/InputParser.java
+++ src/main/java/org/rommert/adventofcode2021/InputParser.java
@@ -7,10 +7,20 @@ import java.util.stream.Collectors;
 
 public class InputParser<T> {
 
+    private String inputSeparator;
+
+    public InputParser() {
+        this("\n");
+    }
+
+    public InputParser(String inputSeparator) {
+        this.inputSeparator = inputSeparator;
+    }
+
     // Expects a String of \n-separated input strings, and a converter to transform
     // each of those input Strings to a T java class, returning the list of all T input
     public List<T> convertInput(String totalInputString, Function<String, T> converterMethod) {
-        return Arrays.stream(totalInputString.split("\n"))
+        return Arrays.stream(totalInputString.split(inputSeparator))
                 .map(converterMethod)
                 .collect(Collectors.toList());
     }
